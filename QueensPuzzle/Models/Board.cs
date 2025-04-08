05f0c641f935b54eb78df01a19d3d76fcb1d5f7d
Max Nolting <max.nolting@viscom.de>
2025-04-08 12:45:52 +0200

    Lesbarkeit

--- QueensPuzzle/Models/Board.cs
+++ QueensPuzzle/Models/Board.cs
@@ -10,14 +10,15 @@ public class Board(int numberQueens, IEnumerable<int> placedQueens)
 
     public async IAsyncEnumerable<int[]> GetSubSolutionsAsync()
     {
-        int queenNumber = PlacedQueens.Count - 1;
+        int queenRowIndex = PlacedQueens.Count - 1;
+        //Hole alle Spalten, in die Dame nicht gesetzt werden darf
         var diagonallyForbidden = PlacedQueens
             .Select(x =>
             {
                 if (x == PlacedQueens.Last())
                     return -1;
                 var rowNumber = PlacedQueens.FindIndex(y => y == x);
-                var rowDiff = queenNumber - rowNumber;
+                var rowDiff = queenRowIndex - rowNumber;
                 return x - rowDiff;
             })
             .Union(
@@ -26,26 +27,34 @@ public class Board(int numberQueens, IEnumerable<int> placedQueens)
                     if (x == PlacedQueens.Last())
                         return -1;
                     var rowNumber = PlacedQueens.FindIndex(y => y == x);
-                    var rowDiff = queenNumber - rowNumber;
+                    var rowDiff = queenRowIndex - rowNumber;
                     return x + rowDiff;
                 })
             );
-        if (PlacedQueens.Count() > 0)
+
+        //wenn Dame nicht platziert werden darf, gib keine Unterlösungen zurück
+        if (PlacedQueens.Count != 0)
         {
             if (diagonallyForbidden.Contains(PlacedQueens.Last()))
             {
                 yield break;
             }
         }
+        
+        //wenn richtige Anzahl Damen platziert, gib diese Konfiguration zurück
         if (NumberQueens == PlacedQueens.Count)
             yield return PlacedQueens.ToArray();
 
+        //Hole alle möglichen Boards nach platzierter Dame
         var nextBoardConfigs = Enumerable.Range(0, NumberQueens)
             .Where(x => !PlacedQueens.Contains(x))
             .Select(x => new Board(numberQueens, [.. PlacedQueens, x]));
+
+        //Hole für jedes der Boards rekursiv die Lösungen
         var nextSolutions = nextBoardConfigs
             .Select(x => x.GetSubSolutionsAsync());
 
+        //Gib alle Lösungen zurück
         foreach (var results in nextSolutions)
         {
             await foreach (var solution in results)
